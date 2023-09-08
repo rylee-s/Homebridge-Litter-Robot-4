60940a687f177e553ddbe2ee33355c5c7ab4c3c6
Rylee <rylee.shearer@gmail.com>
2023-09-08 02:16:34 -0600

    Homebridge configs

---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+/**
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ */
+export const PLATFORM_NAME = 'LitterRobot4';
+
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'homebridge-litter-robot-4';
