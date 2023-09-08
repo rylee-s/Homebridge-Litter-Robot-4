e861df88e2cfdffda6472701415d53e9dd8660ec
Rylee <rylee.shearer@gmail.com>
2023-09-08 02:16:06 -0600

    Define config and add device discovey + init logic

---
+++ src/platform.ts
@@ -0,0 +1,114 @@
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import Whisker from './api/Whisker';
+import { GlobeLightAccessory } from './accessories/globeLight';
+
+/**
+ * HomebridgePlatform
+ * This class is the main constructor for your plugin, this is where you should
+ * parse the user config and discover/register accessories with Homebridge.
+ */
+export class LitterRobotPlatform implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service = this.api.hap.Service;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: PlatformAccessory[] = [];
+
+  constructor(
+    public readonly log: Logger,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    const account = new Whisker(this.config, this.log, this.accessories, this.api);
+
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      account.authenticate().then(() => {
+        this.log.debug('Authenticated now discovering devices');
+        this.discoverDevices(account);
+      });
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
+    this.accessories.push(accessory);
+  }
+
+  /**
+   * This is an example method showing how to register discovered accessories.
+   * Accessories must only be registered once, previously created accessories
+   * must not be registered again to prevent "duplicate UUID" errors.
+   */
+  async discoverDevices(account: Whisker) {
+    const data = JSON.stringify({
+      query: `{
+        query: getLitterRobot4ByUser(userId: "${account.accountId}") {
+            serial
+            name
+            isNightLightLEDOn
+        }
+    
+    }`,
+    });
+    account.sendCommand(data).then((response) => {
+      this.log.debug('Discovered devices:', JSON.stringify(response.data.data.query));
+      let devices = response.data.data.query;
+      devices ||= [];
+
+      // loop over the discovered devices and register each one if it has not already been registered
+      for (const device of devices) {
+        // generate a unique id for the accessory
+        const uuid = this.api.hap.uuid.generate(device.serial);
+
+        // see if an accessory with the same uuid has already been registered and restored from
+        // the cached devices we stored in the `configureAccessory` method above
+        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+
+        if (existingAccessory) {
+          // the accessory already exists
+          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+          new GlobeLightAccessory(this, existingAccessory, account, device.serial);
+
+          // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
+          // remove platform accessories when no longer present
+          // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+          // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+        } else {
+          // the accessory does not yet exist, so we need to create it
+          this.log.info('Adding new accessory:', device.name);
+
+          // create a new accessory
+          const accessory = new this.api.platformAccessory(device.name, uuid);
+
+          // store a copy of the device object in the `accessory.context`
+          // the `context` property can be used to store any data about the accessory you may need
+          accessory.context.device = device;
+
+          // create the accessory handler for the newly create accessory
+          // this is imported from `platformAccessory.ts`
+          new GlobeLightAccessory(this, accessory, account, device.serial);
+
+          // link the accessory to your platform
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
+      }
+    });
+  }
+}
